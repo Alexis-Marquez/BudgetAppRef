781356f40d28e2448fc6089f03dbdb8049a837dc
Alexis Marquez <alexismarq337@gmail.com>
2024-12-02 22:44:23 -0700

    Initialized project

---
+++ src/main/java/org/example/budgetapprefactored/BudgetAppRefactoredApplication.java
@@ -0,0 +1,13 @@
+package org.example.budgetapprefactored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAppRefactoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetAppRefactoredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/budgetapprefactored/BudgetAppRefactoredApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.budgetapprefactored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAppRefactoredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
